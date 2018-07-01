bf9cfcc98d9f9dfbef2987ebe7b1a7a4727af6e9
Cloud-Jeong <cloud.jung@gmail.com>
2018-07-01 20:59:33 +0900

    Prevent yum auto upgrade

--- cmd/kcem/app/cmd/create.go
+++ cmd/kcem/app/cmd/create.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"github.com/cloud-jeong/go-startup/cmd/kcem/app/cmd/options"
 	"fmt"
-	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
@@ -13,23 +12,15 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-func init() {
-//	fmt.Println("Called init() ..")
-}
-
 // NewCmdStatus returns "kcem status" command.
-func NewCmdStatus(out io.Writer) *cobra.Command {
+func NewCmdCreate(out io.Writer) *cobra.Command {
 
 	queryOptions := &options.Status{}
 
 	cmd := &cobra.Command{
-		Use:   "status",
-		Short: "Run this command to get current cluster status",
+		Use:   "create",
+		Short: "Run this command to create k8s Deployment",
 		Run: func(cmd *cobra.Command, args []string) {
-
-			cmd.Flags().Visit(func(f *pflag.Flag) {
-				fmt.Printf("option [%s][%v]\n", f.Name, f.Value)
-			})
 
 			config, err := clientcmd.BuildConfigFromFlags("", "/Users/cloud/.kube/acorn_kube_config")
 			if err != nil {
@@ -40,24 +31,6 @@ func NewCmdStatus(out io.Writer) *cobra.Command {
 			if err != nil {
 				panic(err.Error())
 			}
-
-			pods, err := clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			fmt.Printf("kube-system %d pods running\n", len(pods.Items))
-
-			pods, err = clientset.CoreV1().Pods("cocktail-system").List(metav1.ListOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			fmt.Printf("cocktail-system %d pods running\n", len(pods.Items))
-
-			pods, err = clientset.CoreV1().Pods("monitoring").List(metav1.ListOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			fmt.Printf("monitoring %d pods running\n", len(pods.Items))
 
 			deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
 
@@ -115,4 +88,3 @@ func NewCmdStatus(out io.Writer) *cobra.Command {
 	return cmd
 }
 
-func int32Ptr(i int32) *int32 { return &i }
